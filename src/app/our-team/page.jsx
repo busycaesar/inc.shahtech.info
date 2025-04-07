f73389b83ffaf314016a03aa1d6e344f678b2be3
Dev <busycaesar@gmail.com>
2025-04-07 16:39:55 -0400

    Make Team page responsive

--- src/app/our-team/page.jsx
+++ src/app/our-team/page.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import SuperScriptLogo from "@/components/common/super-script-logo";
 import Image from "next/image";
 import DevProfile from "@/../public/dev-profile.jpeg";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function OurTeam() {
   return (
@@ -10,18 +13,28 @@ export default function OurTeam() {
         Our Team
         <SuperScriptLogo className="md:w-30 w-15" />
       </h1>
-      <div className="flex flex-col md:flex-row items-center md:items-start gap-6 max-w-6xl mx-auto p-6 bg-white shadow-lg rounded-xl">
-        <div className="md:w-200 rounded-full overflow-hidden border-3 border-black">
-          <Image
-            src={DevProfile}
-            alt="Dev"
-            className="w-full h-full object-cover"
-          />
-        </div>
-        <div className="text-center md:text-left">
-          <h2 className="text-[2em] font-bold">Dev J. Shah</h2>
-          <p className="text-[1.5em] font-semibold">AI Evangelist</p>
-          <p className="mt-2 text-gray-600">
+      <Card>
+        <CardHeader className="text-center">
+          <div className="flex flex-col md:flex-row gap-6 items-center">
+            <div className="rounded-full overflow-hidden border-2 border-black">
+              <Image
+                src={DevProfile}
+                alt="Dev"
+                className="w-64 object-cover rounded-full"
+              />
+            </div>
+            <div className="text-center md:text-left">
+              <h2 className="text-[2em] font-bold">Dev J. Shah</h2>
+              <p className="text-[1.5em] font-semibold">AI Evangelist</p>
+              <div className="flex gap-2 justify-center my-3">
+                <Button className="text-xl">Blogs</Button>
+                <Button className="text-xl">Videos</Button>
+              </div>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <p>
             Dev creates all content and manages every aspect of Developer
             Relations at ShahTech Inc. This includes writing technical blogs,
             producing video tutorials, delivering presentations at conferences
@@ -30,8 +43,8 @@ export default function OurTeam() {
             Inc are executed with consistency, professionalism, and a deep
             understanding of both developer needs and product goals.
           </p>
-        </div>
-      </div>
+        </CardContent>
+      </Card>
     </>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardFooter,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import SuperScriptLogo from "@/components/common/super-script-logo";
 
 export default function Home() {
